3ceb857ac3a4eadaa9ccb956b8533c67c45959f9
Niek Palm <dev.npalm@gmail.com>
2019-06-20 22:33:28 +0200

    Update users

--- 05-delegate-global-stub-coredns.sh
+++ 05-delegate-global-stub-coredns.sh
@@ -4,4 +4,4 @@ set -x
 
 ISTIO_DNS_IP="$(kubectl -n istio-system get service istiocoredns -o jsonpath='{.spec.clusterIP}')"
 
-kubectl apply -f <(cat 05x-coredns-global-stub.yaml | sed "s/ISTIO_DNS_IP/${ISTIO_DNS_IP}/")
+cat 05x-coredns-global-stub.yaml | sed "s/ISTIO_DNS_IP/${ISTIO_DNS_IP}/" | kubectl apply -f -
--- 06-bookinfo-deploy-1.sh
+++ 06-bookinfo-deploy-1.sh
@@ -1,6 +1,10 @@
 source 00-common.sh
 
+env
+
+echo $ISTIO_VERSION
+exit 1
 set -x
-kubectl apply -f istio/samples/bookinfo/platform/kube/bookinfo-a.yaml
-kubectl apply -f istio/samples/bookinfo/networking/bookinfo-gateway.yaml
+kubectl apply -f istio-$ISTIO_VERSION/samples/bookinfo/platform/kube/bookinfo-a.yaml
+kubectl apply -f istio-$ISTIO_VERSION/samples/bookinfo/networking/bookinfo-gateway.yaml
 kubectl get pods --watch
